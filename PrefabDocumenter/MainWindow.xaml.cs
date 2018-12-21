92e5c56a68965300cf5d07064a40b0c7e542ec65
pf35301 <pf35301@gmail.com>
2018-12-21 16:17:54 +0900

    [clean] nuget packages

--- PrefabDocumenter/MainWindow.xaml.cs
+++ PrefabDocumenter/MainWindow.xaml.cs
@@ -7,18 +7,6 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Xml.Linq;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using exiii;
-using AngleSharp.Dom.Html;
-using AngleSharp.Html;
-using System.Text.RegularExpressions;
-using System.Collections.ObjectModel;
 using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace PrefabDocumenter
